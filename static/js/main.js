dedf8ab5746894cf361ae3d4008861eb125722c1
Jessica Janiuk <jessica.janiuk@gmail.com>
2013-04-12 10:04:49 -0600

    Removed unnecessary files

--- static/js/main.js
+++
@@ -1,9 +0,0 @@
-define(
-	['jquery', '/js/libs/modernizr.min.js', '/js/plugins.js'],
-	function($, Modernizr, bootstrap){
-		console.log($);
-		console.log(Modernizr);
-		console.log(bootstrap);
-		return $;
-	}
-);
