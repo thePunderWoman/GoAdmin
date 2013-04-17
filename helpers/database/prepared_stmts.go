2615ac020b513d8ea3a6d2a2d5ee7abad29b72d7
Jessica Janiuk <jessica.janiuk@gmail.com>
2013-04-17 08:40:41 -0600

    Cleaned up the adding and preparing of sql statements

--- helpers/database/prepared_stmts.go
+++ helpers/database/prepared_stmts.go
@@ -8,23 +8,21 @@ import (
 
 // prepared statements go here
 var (
-	// example statement
-	authenticateUserStmt  = "select * from user where username=? and encpassword=? and isActive = 1"
-	getUserByIDStmt       = "select * from user where id=?"
-	getUserByUsernameStmt = "select * from user where username=?"
-	getUserByEmailStmt    = "select * from user where email=?"
-	allUserStmt           = "select * from user"
-	userModulesStmt       = "select module.* from module inner join user_module on module.id = user_module.moduleID where user_module.userID = ? order by module"
-	setUserPasswordStmt   = "update user set encpassword = ? where id = ?"
-)
-
-// Create map of all statements
-var (
 	Statements map[string]mysql.Stmt
 )
 
 // Prepare all MySQL statements
 func PrepareAll() error {
+
+	UnPreparedStatements := make(map[string]string, 0)
+
+	UnPreparedStatements["authenticateUserStmt"] = "select * from user where username=? and encpassword=? and isActive = 1"
+	UnPreparedStatements["getUserByIDStmt"] = "select * from user where id=?"
+	UnPreparedStatements["getUserByUsernameStmt"] = "select * from user where username=?"
+	UnPreparedStatements["getUserByEmailStmt"] = "select * from user where email=?"
+	UnPreparedStatements["allUserStmt"] = "select * from user"
+	UnPreparedStatements["userModulesStmt"] = "select module.* from module inner join user_module on module.id = user_module.moduleID where user_module.userID = ? order by module"
+	UnPreparedStatements["setUserPasswordStmt"] = "update user set encpassword = ? where id = ?"
 
 	Statements = make(map[string]mysql.Stmt, 0)
 
@@ -32,50 +30,19 @@ func PrepareAll() error {
 		AdminDb.Connect()
 	}
 
-	// Example Preparation
-	authenticateUserPrepared, err := AdminDb.Prepare(authenticateUserStmt)
-	if err != nil {
-		return err
+	for stmtname, stmtsql := range UnPreparedStatements {
+		stmt, err := PrepareStatement(stmtsql)
+		if err == nil {
+			Statements[stmtname] = stmt
+		}
 	}
-	Statements["authenticateUserStmt"] = authenticateUserPrepared
-
-	allUserPrepared, err := AdminDb.Prepare(allUserStmt)
-	if err != nil {
-		return err
-	}
-	Statements["allUserStmt"] = allUserPrepared
-
-	getUserByIDPrepared, err := AdminDb.Prepare(getUserByIDStmt)
-	if err != nil {
-		return err
-	}
-	Statements["getUserByIDStmt"] = getUserByIDPrepared
-
-	getUserByUsernamePrepared, err := AdminDb.Prepare(getUserByUsernameStmt)
-	if err != nil {
-		return err
-	}
-	Statements["getUserByUsernameStmt"] = getUserByUsernamePrepared
-
-	getUserByEmailPrepared, err := AdminDb.Prepare(getUserByEmailStmt)
-	if err != nil {
-		return err
-	}
-	Statements["getUserByEmailStmt"] = getUserByEmailPrepared
-
-	userModulesPrepared, err := AdminDb.Prepare(userModulesStmt)
-	if err != nil {
-		return err
-	}
-	Statements["userModulesStmt"] = userModulesPrepared
-
-	setUserPasswordPrepared, err := AdminDb.Prepare(setUserPasswordStmt)
-	if err != nil {
-		return err
-	}
-	Statements["setUserPasswordStmt"] = setUserPasswordPrepared
 
 	return nil
+}
+
+func PrepareStatement(sql string) (stmt mysql.Stmt, err error) {
+	stmt, err = AdminDb.Prepare(sql)
+	return
 }
 
 func GetStatement(key string) (stmt mysql.Stmt, err error) {
